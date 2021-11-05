3e3e43cce5adb4f2acc9e8b28ab5ac00abca0808
wendelbs <wendelbs14@gmail.com>
2021-11-04 23:22:48 -0300

    Primeiro commit final

---
+++ teste.js
@@ -0,0 +1,9 @@
+function validArray(arr, num){
+  if(!arr && !num)throw new ReferencEros("");
+  if (typeof arr !='object') throw new TypeError("");
+  if(typeof n!='number') throw new TypeError("");
+}
+
+if(x != 10){
+  throw new TypeError("Houve um erro")
+}
